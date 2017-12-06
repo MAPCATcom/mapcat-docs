8c5e5d2cb26ca7288b850dbc4dcb63d5be7fc325
Kapcsándi István <ikapcsandi@mapcat.com>
2017-12-06 17:20:55 +0100

    fix fluid position of footer

--- mapcat-gray/assets/js/page.js
+++ mapcat-gray/assets/js/page.js
@@ -1,1 +1,15 @@
-$('.main table').addClass('table');
+let resize = function() {
+    let padding = 50;
+    let footHeight = $('footer').height();
+    let pageWidth = $(window).width();
+    if (pageWidth < 768) {
+        $('body').css('margin-bottom',0);
+    } else {
+        $('body').css('margin-bottom', (padding + footHeight) + 'px');
+    }
+}
+$(window).on('resize', resize);
+$(document).ready(function() {
+    resize();
+    $('.main table').addClass('table');
+});

b97aea408465195048c9c398cdceef0df51f6a99
Milán Pintér <mpinter@mapcat.com>
2018-01-11 10:53:20 +0100

    mapbox demo update

--- mapbox-demo/script.js
+++ mapbox-demo/script.js
@@ -1,12 +1,53 @@
+var map = undefined;
 
-function initMap() {
+function initMap(style) {
     mapboxgl.accessToken = 'undefined';
     var map = new mapboxgl.Map({
         container: 'map',
-        style: '/demo/mapbox/assets/mapcat.json',
+        style: style,
         center: [0, 51.5],
         zoom: 13
     });
 }
 
-initMap();
+function load(token) {
+    mapcatview.initVectorView(token, {layer: { "base":"", "ocean":"" }}, function(error, response) {
+        if (error) {
+            map = undefined;
+            $('#map').empty();
+            if (typeof(error) === 'object') {
+                $('#warningMessage').html(error.message);
+            } else {
+                $('#warningMessage').html(error);
+            }
+            console.log(error);
+        } else {
+            initMap(response);
+            $('#apikeyModal').modal('hide');
+        }
+    });
+}
+
+$(document).ready( function() {
+    $('#apikeyModal').modal('show');
+
+    $('#submitButton').on('click', function() {
+        load($('#apiKey').val());
+    });
+
+    $('#apikeyModal').on('hidden.bs.modal', function () {
+        if (map === undefined) {
+            $('#message').html('You need a MAPCAT access token to show the example map.');
+            $('#modalButton').show();
+        } else {
+            $('#modalButton').hide();
+            $('#message').html('');
+        }
+    });
+
+    $('#modalButton').on('click', function() {
+        $('#modalButton').hide();
+        $('#message').html('');
+        $('#warningMessage').html('');
+    });
+});
